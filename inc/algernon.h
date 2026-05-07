89cf8f1554aed05883ef0ccb0a9dd62206f6c7fc
王琛 <wanggreatwall@foxmail.com>
2026-05-08 01:48:27 +0800

    [style] (inc): 修改 clang-format, 并修改部分头文件格式;

--- inc/algernon.h
+++ inc/algernon.h
@@ -6,23 +6,23 @@
  * @brief Umbrella header: include this single file to import all Algernon modules.
  */
 
-#include "version.h"
+#include "cv/ximage.h"
+#include "cv/ximage_io.h"
+#include "file/xfile.h"
+#include "file/xpath.h"
+#include "json/xjson.h"
 #include "log/xerror.h"
 #include "log/xlogger.h"
 #include "math/xmath.h"
 #include "memory/xbuffer.h"
-#include "file/xpath.h"
-#include "file/xfile.h"
-#include "regex/xregex.h"
-#include "json/xjson.h"
 #include "perf/xtimer.h"
 #include "perf/xtracer.h"
+#include "regex/xregex.h"
 #include "sys/xdlib.h"
 #include "sys/xplatform.h"
-#include "cv/ximage.h"
-#include "cv/ximage_io.h"
-#include "util/xargs.h"
-#include "threadpool/xthreadpool.h"
 #include "threadpool/xthread_flow.h"
+#include "threadpool/xthreadpool.h"
+#include "util/xargs.h"
+#include "version.h"
 
-#endif // ALGERNON_H_
+#endif  // ALGERNON_H_
